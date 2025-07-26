0cc8a508ba9c6b851f8f8619612740c56f3e5c7b
evanson <evansonmwangi83@gmail.com>
2025-07-26 09:01:25 +0300

    chore(terraform): rename cluster name

--- locals.tf
+++ locals.tf
@@ -3,7 +3,7 @@ data "aws_availability_zones" "available" {}
 locals {
   env             = var.environment
   region          = var.aws_region
-  cluster_name    = "${var.environment}-${var.cluster_name}-cluster"
+  cluster_name    = "${var.cluster_name}-cluster"
   cluster_version = var.cluster_version
 
   azs   = slice(data.aws_availability_zones.available.names, 0, 3)
