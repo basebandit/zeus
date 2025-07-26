3d4d6f300a1f37f9c4cc851d8ff49f43f00bc927
evanson <evansonmwangi83@gmail.com>
2025-07-26 19:30:58 +0300

    chore(terraform): refactor and add helm provider

--- infrastructure/staging/providers.tf
+++ infrastructure/staging/providers.tf
@@ -1,7 +1,22 @@
+data "aws_eks_cluster" "eks" {
+  name = aws_eks_cluster.eks.name
+}
+
+data "aws_eks_cluster_auth" "eks" {
+  name = aws_eks_cluster.eks.name
+}
+
+provider "helm" {
+  kubernetes = {
+    host                   = data.aws_eks_cluster.eks.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
+    token                  = data.aws_eks_cluster_auth.eks.token
+  }
+}
+
 provider "aws" {
   region = local.region
 }
-
 
 terraform {
   required_version = ">= 1.0"
@@ -10,6 +25,11 @@ terraform {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.49"
+    }
+
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 3.0.2"
     }
   }
 }
